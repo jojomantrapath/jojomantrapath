df18f117347d1d367692c5320ccd813a453b5bc3
jojoMantraMind <Jordan.Neumann@gmail.com>
2023-08-31 19:19:56 -0400

    @jsneum01: james web background

--- jojomantrapath/src/App.tsx
+++ jojomantrapath/src/App.tsx
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bodyContainer d-flex flex-column w-100">
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
