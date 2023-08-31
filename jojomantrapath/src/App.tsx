482041c0315e8b12c2395012d9a1a2dd41dc9f44
jojoMantraMind <Jordan.Neumann@gmail.com>
2023-08-31 19:42:22 -0400

    @jsneum01: app initialization with logo and header

--- jojomantrapath/src/App.tsx
+++ jojomantrapath/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import mainLogo from "./media/jojomantrapath.png";
 
 function App() {
   return (
-    <div className="bodyContainer d-flex flex-column w-100">
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="body-container d-flex flex-column align-items-center">
+      <div className="logo-container d-flex w-100 justify-content-center pt-5 pb-3">
+        <img
+          src={mainLogo}
+          alt="jojomantrapath logo"
+          className="w-160px h-160px"
+        />
+      </div>
+
+      <div className="title-container d-flex pb-3">
+        <h1 className="text-white font-size-24px shadow">jojoMantraPath</h1>
+      </div>
+
+      <div className="audio-container d-flex justify-content-center">
+        <audio controls className="w-100"></audio>
+      </div>
+
+      <div className="content-container d-flex flex-column w-100 px-3">
+        <div className="intro-container d-flex justify-content-center">
+          <h2 className="text-white font-size-16px">Jordan Neumann-Burns</h2>
+        </div>
+
+        <div className="intro-container"></div>
+      </div>
     </div>
   );
 }
