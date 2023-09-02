37a1b0d6f869b92e1d7b9c002101912287f9a25d
jojoMantraMind <Jordan.Neumann@gmail.com>
2023-09-02 10:16:17 -0400

    @jojomantrapath: initial layout for collapsibles

--- jojomantrapath/src/App.tsx
+++ jojomantrapath/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import mainLogo from "./media/jojomantrapath.png";
+import mainAudio from "./media/Only-One-Bar-in-Interstellar-Space.mp3";
 
 function App() {
   return (
@@ -15,19 +16,99 @@ function App() {
       </div>
 
       <div className="title-container d-flex pb-3">
-        <h1 className="text-white font-size-24px shadow">jojoMantraPath</h1>
+        <h1 className="text-white font-size-24px shadow mb-0">
+          jojoMantraPath
+        </h1>
       </div>
 
-      <div className="audio-container d-flex justify-content-center">
-        <audio controls className="w-100"></audio>
+      <div className="audio-container d-flex justify-content-center w-100 px-3 pb-3">
+        <audio controls className="w-100" src={mainAudio}></audio>
       </div>
 
       <div className="content-container d-flex flex-column w-100 px-3">
         <div className="intro-container d-flex justify-content-center">
-          <h2 className="text-white font-size-16px">Jordan Neumann-Burns</h2>
+          <h2 className="text-white font-size-18px shadow mb-0">
+            Jordan Neumann-Burns
+          </h2>
         </div>
 
-        <div className="intro-container"></div>
+        <div className="intro-container" id="accordion">
+          <div className="about-card card">
+            <div className="card-header" id="headingOne">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link text-white font-size-18px shadow"
+                  data-toggle="collapse"
+                  data-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  <span className="">About</span>
+                </button>
+              </h5>
+            </div>
+          </div>
+          <div className="experience-card card">
+            <div className="card-header" id="headingTwo">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link text-white font-size-18px shadow"
+                  data-toggle="collapse"
+                  data-target="#collapseTwo"
+                  aria-expanded="true"
+                  aria-controls="collapseTwo"
+                >
+                  <span className="">Experience</span>
+                </button>
+              </h5>
+            </div>
+          </div>
+          <div className="education-card card">
+            <div className="card-header" id="headingThree">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link text-white font-size-18px shadow"
+                  data-toggle="collapse"
+                  data-target="#collapseThree"
+                  aria-expanded="true"
+                  aria-controls="collapseThree"
+                >
+                  <span className="">Education</span>
+                </button>
+              </h5>
+            </div>
+          </div>
+          <div className="skills-card card">
+            <div className="card-header" id="headingFour">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link text-white font-size-18px shadow"
+                  data-toggle="collapse"
+                  data-target="#collapseFour"
+                  aria-expanded="true"
+                  aria-controls="collapseFour"
+                >
+                  <span className="">Skills</span>
+                </button>
+              </h5>
+            </div>
+          </div>
+          <div className="interests-card card">
+            <div className="card-header" id="headingFive">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link text-white font-size-18px shadow"
+                  data-toggle="collapse"
+                  data-target="#collapseFive"
+                  aria-expanded="true"
+                  aria-controls="collapseFive"
+                >
+                  <span className="">Interests</span>
+                </button>
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
